fb6858aba04e9ee7cdeebcc9b3e0bbf1f867cbf4
Steve Albonico <salbonico@gmail.com>
2019-01-29 20:52:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,6 +30,6 @@ class Student
     VALUES (?,?);
     SQL
    DB[:conn].execute(sql,@name,@grade)
-   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 end
